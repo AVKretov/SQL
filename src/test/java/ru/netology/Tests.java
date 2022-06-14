cc60cc4c47c428049867261dc75b8447fd61863a
Aleksandr Kretov <kretov_a@outlook.com>
2022-06-14 09:22:52 +0700

    without negative test

--- src/test/java/ru/netology/Tests.java
+++ src/test/java/ru/netology/Tests.java
@@ -2,41 +2,45 @@ package ru.netology;
 
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 import java.sql.DriverManager;
 
+
 class Tests {
-//    @AfterAll
-//    void deleteDb() throws SQLException {
-//        String codeSQL = "DROP TABLE IF EXISTS cards; DROP TABLE IF EXISTS auth_codes;" +
-//                " DROP TABLE IF EXISTS card_transactions; DROP TABLE IF EXISTS users;";
-//        try (
-//                var conn = DriverManager.getConnection(
-//                        "jdbc:mysql://localhost:3307/app", "app", "pass"
-//                );
-//                var stmt = conn.createStatement();
-//        ) {
-//            stmt.execute(codeSQL);
-//
-//        }
-//
-//    }
+    @AfterAll
+    public static void deleteDb() throws SQLException {
+        String delCards = "TRUNCATE cards;";
+        String delCardTrans = "TRUNCATE card_transactions;";
+        String delAuthCodes = "TRUNCATE auth_codes;";
+        String delUsers = "DELETE FROM users;";
+        try (
+                var conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3307/app", "app", "pass"
+                );
+                var stmt = conn.createStatement();
+        ) {
+            stmt.executeUpdate(delCards);
+            stmt.executeUpdate(delCardTrans);
+            stmt.executeUpdate(delAuthCodes);
+            stmt.executeUpdate(delUsers);
+
+
+        }
+
+    }
 
     @BeforeEach
     void setUp(){
         open("http://localhost:9999");
         $("[data-test-id='login'] .input__control").setValue("Vasya");
     }
+
     @Test
     void happyPath() throws SQLException {
 
@@ -60,14 +64,15 @@ class Tests {
 
 
     }
-    @Test
-    void negativePath(){
-        $("[data-test-id='password'] .input__control").setValue("111");
-        for (int i = 0; i < 3; i++)
-            $("[data-test-id='action-login']").click();
-        //все работает, а не должно
+//    @Test
+//    void negativePath(){
+//        $("[data-test-id='password'] .input__control").setValue("111");
+//        for (int i = 0; i < 3; i++)
+//            $("[data-test-id='action-login']").click();
+//
+//
+//    }
 
-    }
 
 
 
